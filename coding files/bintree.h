8c04f1a1d1e302d19953e2a85816944ac881d0a6
7ntys <61086353+7ntys@users.noreply.github.com>
2022-10-26 13:19:51 +0200

    Implementing Basic structure (Seance Projet 1)

--- coding files/bintree.h
+++ coding files/bintree.h
@@ -1,8 +1,10 @@
 //
 // Created by Julien Le ber on 21/10/2022.
 //
-
+#include "node.h"
 #ifndef PROJET_C_RANDOM_SENTENCES_BINTREE_H
 #define PROJET_C_RANDOM_SENTENCES_BINTREE_H
-
+struct tree{
+    p_node root;
+};
 #endif //PROJET_C_RANDOM_SENTENCES_BINTREE_H
--- coding files/node.h
+++ coding files/node.h
@@ -1,8 +1,22 @@
 //
 // Created by Julien Le ber on 21/10/2022.
 //
-
+#include <stdio.h>
 #ifndef PROJET_C_RANDOM_SENTENCES_NODE_H
 #define PROJET_C_RANDOM_SENTENCES_NODE_H
-
+#define max 27
+struct node{
+    //commmun part
+    char c;
+    struct node* children[max];
+    int sons;
+    //Part Verb
+    char verbe_flechie[6][6];
+    //Part nom
+    char nom_flechies[2];
+    int gender;
+    //Part adjective
+    char adjective_flechies[2][2];
+    //Part adverb
+};typedef struct node* p_node;
 #endif //PROJET_C_RANDOM_SENTENCES_NODE_H
